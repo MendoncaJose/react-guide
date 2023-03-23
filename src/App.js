ca9a3e2cb2d5ed3e966c502f3948227d24e46105
MendoncaJose <103999073+MendoncaJose@users.noreply.github.com>
2023-03-23 18:12:16 +0000

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import NewExpense from './components/NewExpense/NewExpense';
+import Expenses from './components/Expenses';
+
+function App() {
+  const expenses =[
+    {title: 'engine', 
+    amount: 300.12, 
+    date: new Date(2023, 3, 23)},
+    {title: 'eletrical bill', 
+    amount: 30.2, 
+    date: new Date(2023, 3, 23)},
+    {title: 'oil', 
+    amount: 30, 
+    date: new Date(2023, 3, 23)},
+    {title: 'gas', 
+    amount: 100, 
+    date: new Date(2023, 3, 23)},
+  ];
+  return (
+    <div>
+      <NewExpense />
+      <Expenses expenses={expenses}/>
+    </div>
+  );
+}
+
+export default App;
