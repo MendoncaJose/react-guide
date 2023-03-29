7e141349b7dff62dc137a1ab162ef280c4da40c7
MendoncaJose <mendoncajose7@gmail.com>
2023-03-29 16:31:52 +0100

    Finished Expenses
    Finito

--- src/App.js
+++ src/App.js
@@ -1,29 +1,30 @@
-import React, { useState } from "react";
-import NewExpense from "./components/NewExpense/NewExpense";
-import Expenses from "./components/Expenses";
+import React, { useState } from 'react';
+
+import NewExpense from './components/NewExpense/NewExpense';
+import Expenses from './components/Expenses/Expenses';
 
 const DUMMY_EXPENSES = [
-  { id: 'e1',
-  title: "engine", 
-  amount: 300.12, 
-  date: new Date(2023, 3, 23) 
+  {
+    id: 'e1',
+    title: 'Toilet Paper',
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
   },
-  { id: 'e2',
-  title: "eletrical bill", 
-  amount: 30.2, 
-  date: new Date(2023, 3, 23) 
+  { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+  {
+    id: 'e3',
+    title: 'Car Insurance',
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
   },
-  { id: 'e3',
-    title: "oil", 
-  amount: 30, 
-  date: new Date(2023, 3, 23) 
-  },
-  { id: 'e4',
-    title: "gas", 
-  amount: 100, 
-  date: new Date(2023, 3, 23) 
+  {
+    id: 'e4',
+    title: 'New Desk (Wooden)',
+    amount: 450,
+    date: new Date(2021, 5, 12),
   },
 ];
+
 const App = () => {
   const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
 
@@ -33,10 +34,17 @@ const App = () => {
     });
   };
 
+  // return React.createElement(
+  //   'div',
+  //   {},
+  //   React.createElement('h2', {}, "Let's get started!"),
+  //   React.createElement(Expenses, { items: expenses })
+  // );
+
   return (
     <div>
       <NewExpense onAddExpense={addExpenseHandler} />
-      <Expenses expenses={expenses} />
+      <Expenses items={expenses} />
     </div>
   );
 };
